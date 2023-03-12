b7fc77ce53f918361cef2f1d64946b30ab4a5a1d
yukseldemirtas <yukseldemirtas18@hotmail.com>
2023-03-12 14:36:14 +0300

    Add project files.

---
+++ AirportDistance_WebAPI/Controllers/AirportDistance.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirportDistance_WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AirportDistance : ControllerBase
+	{
+	}
+}
